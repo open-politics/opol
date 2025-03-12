0e088f7bd5f62b2191e3f896b948b3fb5b36a82a
Jim Wagner <jimvwagner@googlemail.com>
2025-03-12 03:08:48 +0000

    ✨ super easy boot script

--- opol/stack/boot-local.sh
+++ opol/stack/boot-local.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-# check that HUGGINGFACE_TOKEN in .env.local is set
-HUGGINGFACE_TOKEN=$(grep -oP 'HUGGINGFACE_TOKEN=\K[^ ]+' .env.local)
-if [ -z "$HUGGINGFACE_TOKEN" ]; then
-  echo "HUGGINGFACE_TOKEN is not set. It's needed for the embedding model, we will fade requiring any additional api keys out of the stack."
-  exit 1
-fi
-
-# needed when publishing, currently using
-# docker compose -f compose.local.yml up -d
-
 pip install -q prefect
 
 unset PREFECT_API_URL
-
-docker compose -f compose.flows.yml up -d
 
 prefect config set PREFECT_API_URL="http://localhost:4200/api"
 
