2d796923d8255b1b7914356a946a7067cd407c67
Jim Wagner <jimvwagner@googlemail.com>
2025-03-18 14:07:43 +0000

    updated boot script

--- boot.sh
+++ boot.sh
@@ -1,14 +1,12 @@
+git clone https://github.com/open-politics/opol.git
+cd opol/
+
 mkdir -p ./opol/stack/.store/data/placeholder
-chmod 777 ./opol/stack/.store/data/placeholder
+sudo chmod 777 ./opol/stack/.store/data/placeholder
 
 cd opol/stack
-mv .env.example .env
-mv .env.local.example .env.local
+cp .env.local .env
+sudo docker network create opol-app-stack
+sudo docker compose -f compose.local.yml up --build -d
 
-sysctl -w vm.overcommit_memory=1
-
-docker compose -f compose.local.yml up --build -d
-
-sleep 10
-
-sudo docker exec -it opol-ollama-1 ollama pull llama3.1
+bash boot-local.sh
